ffa0a945bc39c71dee74c6c5315707c4d67472bf
Aldren peshaia <aldrenpeshaia@hotmail.com>
2023-03-13 13:35:09 +0100

    added scanner and objectmapper

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ObjectMapper mapper = new ObjectMapper();
     }
-    Scanner scanner = new Scanner(System.in);
+
 
 }
